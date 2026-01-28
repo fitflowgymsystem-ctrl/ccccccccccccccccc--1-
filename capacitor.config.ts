7e7747d5ba5827f0d23c686d33c762345d900970
fitflowgymsystem-ctrl <fitflow.gymsystem@gmail.com>
2026-01-28 12:30:40 +0200

    .

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fitflow.app',
+  appName: 'FitFlow',
+  webDir: 'dist'
+};
+
+export default config;
